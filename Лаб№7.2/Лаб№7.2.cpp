5d05da0a24431f39ebafd974538d5c537ce4e2f2
Povilika310121 <73997912+Povilika310121@users.noreply.github.com>
2020-11-13 22:04:04 +0300

    Задание №4
    Построить все подмножества
    Антонюк В., 26/1

---
+++ Лаб№7.2/Лаб№7.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    int n = 3;
+    int arr[] = { 0,1,2};
+    int s = pow(2, n);
+        for (int i = 0; i < s; ++i)
+        {
+            cout << "{";
+            for (int j = 0; j < n; ++j)
+                if(i &(1<<j))
+                // & побитовая конъюкция  << побитовый сдвиг влево
+                cout << arr[j] << " ";
+            cout << "}" << endl;
+        }
+    system("pause");
+    return 0;
+}
+
